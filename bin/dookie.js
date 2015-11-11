4ea5def904c461d60013cd4a3408fe9dcb9bcd6c
Valeri Karpov <valkar207@gmail.com>
2015-11-11 15:45:02 -0500

    es6-ify bin/dookie

--- bin/dookie.js
+++ bin/dookie.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+'use strict';
 
-var commander = require('commander');
-var dookie = require('../');
-var fs = require('fs');
-var yaml = require('js-yaml');
+const commander = require('commander');
+const dookie = require('../');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 commander.
   usage('dookie (pull|push) --file <file> --db <database name>').
@@ -11,7 +12,7 @@ commander.
   option('-d, --db <database>', 'Database to read/write from').
   parse(process.argv);
 
-var cmd = process.argv[2];
+const cmd = process.argv[2];
 if (!cmd || ['pull', 'push'].indexOf(cmd) === -1) {
   console.log('Must specify either pull (pull data from db into file) ' +
     'or push (push data into db from file).');
@@ -31,34 +32,27 @@ if (!commander.db) {
 if (cmd === 'pull') {
   console.log('Writing data from ' + commander.db + ' to ' +
     commander.file);
-  dookie.pull('mongodb://localhost:27017/' + commander.db).
-    then(function(data) {
-      fs.writeFileSync(commander.file, JSON.stringify(data, null, '  '));
-      console.log('Success!');
-      process.exit(0);
-    }).
-    catch(function(error) {
-      if (error) {
-        console.log('Error reading data: ' + error);
-        process.exit(1);
-      }
-    });
+  co(function*() {
+    const res = yield dookie.pull(`mongodb://localhost:27017/${commander.db}`);
+    fs.writeFileSync(commander.file, JSON.stringify(data, null, '  '));
+    process.exit(0);
+  }).
+  catch(function(error) {
+    console.log('Error reading data:', error);
+    process.exit(1);
+  });
 } else {
   console.log('Writing data from ' + commander.file + ' to ' +
     commander.db);
-  var data = yaml.safeLoad(fs.readFileSync(commander.file));
 
-  dookie.push('mongodb://localhost:27017/' + commander.db, data).
-    then(() => {
-      if (error) {
-        console.log('Error writing data: ' + error);
-        process.exit(1);
-      }
-      console.log('Success!');
-      process.exit(0);
-    }).
-    catch((error) => {
-      console.log('Error reading data', error);
-      process.exit(1);
-    });
+  co(function*() {
+    const data = yaml.safeLoad(fs.readFileSync(commander.file));
+    yield dookie.push(`mongodb://localhost:27017/${commander.db}`, data);
+
+    console.log('Success!');
+    process.exit(0);
+  }).catch(function(error) {
+    console.log('Error writing data', error);
+    process.exit(1);
+  });
 }
